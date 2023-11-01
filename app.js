bf9caa6dee5bd769c1ea92a9cf776ade789836ec
Ajit Ubbanapally <ajitpurnahk@gmail.com>
2023-11-01 16:46:55 -0500

    choose.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var GiraffeRouter = require('./routes/Giraffe');
 var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
 
 
 var app = express();
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Giraffe', GiraffeRouter);
 app.use('/board', boardRouter);
+app.use('/choose', chooseRouter);
 
 
 
